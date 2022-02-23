f60d78c40faf3473ba43c351ce5073ed6c75905b
mamesjiles@gmail.com <mamesjiles@gmail.com>
2022-02-23 20:33:31 +0000

    eoin

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,16 +1,7 @@
-import sys
-import yfinance as yf  
 import json
-import math
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
+import yfinance as yf  
 from pycoingecko import CoinGeckoAPI
 from datetime import date, datetime
-
 
 def get_historical_data(share_name):
     share = yf.Ticker(share_name)
@@ -49,42 +40,6 @@ def get_crypto_data(id):
     market_cap = [i[1] for i in data["market_caps"]]
 
     return [{"date": str(dates[i]), "price": prices[i], "volume": total_volume[i], "marketCap": market_cap[i]} for i in range(len(dates))]
-
-
-# df = pd.DataFrame.from_dict(data, orient='columns')
-# df["price"].fillna(method="ffill", inplace=True)
-# df["volume"].fillna(method="ffill", inplace=True)
-# df["market_cap"].fillna(method="ffill", inplace=True)
-
-
-# plt.plot(df["price"], color='blue')
-# plt.plot(df["volume"], color='magenta')
-# plt.plot(df["market_cap"], color='black')
-
-# plt.show()
-
-# split_date =  df.index[df['date'] == datetime.strptime("2018-02-25 00:00:00", "%Y-%m-%d %H:%M:%S")].tolist()[0]
-# print("splite_date", split_date, df["date"][split_date])
-# data_train = df[:split_date].copy()
-# data_test = df[split_date:].copy()
-
-# print("len train", len(data_train))
-# print("len test", len(data_test))
-
-
-# print(data_train.head())
-# print(data_test.tail())
-
-# training_set = data_train.values
-# reshape_amt = abs(len(data_train.values) - len(data_test.values))
-# print("train set length", training_set)
-# training_set = np.reshape(training_set, (len(training_set), 1))
-
-# sc = MinMaxScaler()
-# training_set = sc.fit_transform(training_set)
-# X_train = training_set[0:len(training_set) - 1]
-# y_train = training_set[1:len(training_set)]
-# X_train = np.reshape(X_train, (len(X_train), 1, 1))
 
 
 
