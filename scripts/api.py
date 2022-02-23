2b8a11a08f59c2d8b012a719b28d8a55ec2fb10d
mamesjiles@gmail.com <mamesjiles@gmail.com>
2022-02-23 12:01:58 +0000

    .

--- scripts/api.py
+++ scripts/api.py
@@ -17,11 +17,11 @@ def get_cached_data():
 
     return jsonify(data)
 
-@app.route('/<string:ticker>', methods = ['GET'])
+@app.route('/share/<string:ticker>', methods = ['GET'])
 def share_data(ticker):
     if (f"{ticker}.json" not in os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}\data")):
         with open(f"data/{ticker}.json", "w+") as f:
-            json.dump({}, f, indent=4)
+            json.dump({}, f)
     
     with open(f"data/{ticker}.json", "r+") as f:
         all_data = json.load(f)
@@ -30,6 +30,41 @@ def share_data(ticker):
         if (len(historical_data) != 0):
             if ("sector" in share_info):
                 all_data = {
+                    "zip": share_info["zip"],
+                    "city": share_info["city"],
+                    "state": share_info["state"],
+                    "country": share_info["country"],
+                    "website": share_info["website"],
+                    "industry": share_info["industry"],
+                    "sector": share_info["sector"],
+                    "shortName": share_info["shortName"],
+                    "fullTimeEmployees": share_info["fullTimeEmployees"],
+                    "longBusinessSummary": share_info["longBusinessSummary"],
+                    "shortName": share_info["shortName"],
+
+                    "profitMargins": share_info["profitMargins"],
+                    "grossMargins": share_info["grossMargins"],
+                    "totalRevenue": share_info["totalRevenue"],
+                    "totalDebt": share_info["totalDebt"],
+                    "totalCash": share_info["totalCash"],
+                    "grossProfits": share_info["grossProfits"],
+                    "marketCap": share_info["marketCap"],
+                    "revenueGrowth": share_info["revenueGrowth"],
+                    "operatingMargins": share_info["operatingMargins"],
+                    "operatingCashflow": share_info["operatingCashflow"],
+                    "targetLowPrice": share_info["targetLowPrice"],
+                    "targetMedianPrice": share_info["targetMedianPrice"],
+                    "targetMeanPrice": share_info["targetMeanPrice"],
+                    "targetHighPrice": share_info["targetHighPrice"],
+                    "recommendationKey": share_info["recommendationKey"],
+                    "freeCashflow": share_info["freeCashflow"],
+                    "returnOnAssets": share_info["returnOnAssets"],
+                    "numberOfAnalystOpinions": share_info["numberOfAnalystOpinions"],
+                    "totalCashPerShare": share_info["totalCashPerShare"],
+                    "revenuePerShare": share_info["revenuePerShare"],
+                    "bookValue": share_info["bookValue"],
+                    "forwardPE": share_info["forwardPE"],
+                   
                     "currentPrice": get_current_price(ticker),
                     "historicalData": historical_data,
                     "fiftyTwoWeekLow": share_info["fiftyTwoWeekLow"],
@@ -37,13 +72,6 @@ def share_data(ticker):
                     "fiftyDayAverage": share_info["fiftyDayAverage"],
                     "twoHundredDayAverage": share_info["twoHundredDayAverage"],
                     "fiftyTwoWeekChange": share_info["52WeekChange"],
-                    "shortName": share_info["shortName"],
-                    "totalRevenue": share_info["totalRevenue"],
-                    "totalDebt": share_info["totalDebt"],
-                    "totalCash": share_info["totalCash"],
-                    "grossProfits": share_info["grossProfits"],
-                    "industry": share_info["industry"],
-                    "marketCap": share_info["marketCap"],
                 }
             
             else:
@@ -53,7 +81,8 @@ def share_data(ticker):
                     "fiftyTwoWeekHigh": share_info["fiftyTwoWeekHigh"],
                     "fiftyDayAverage": share_info["fiftyDayAverage"],
                     "twoHundredDayAverage": share_info["twoHundredDayAverage"],
-                    "historicalData": historical_data
+                    "historicalData": historical_data,
+                    "shareInfo": share_info
                 }
 
             f.seek(0)
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,49 +2,19 @@ import sys
 import yfinance as yf  
 import json
 import math
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
-import datetime as dt
-from datetime import date
-
-
-def linear_regression(x, y):
-
-    x = np.array([i for i in range(len(x))])
-    y = np.array([i for i in y])
-
-    print("len X", len(x))
-    print("len Y", len(y))
-    
-    x.reshape(len(x), 1)
-    y.reshape(len(y), 1)
-
-    print("X:", x[0:10])
-    print("Y:", y[0:10])
-
-    linreg = LinearRegression().fit(x, y)
-    linreg.score(x, y)
-    predictions = linreg.predict(y)
-    # model = model = LinearRegression().fit(x, y)
-    # r_sq = model.score(x, y)
-    # print('coefficient of determination:', r_sq)
-
-    # y_new = np.arange(20).reshape((-1, 1))
-    # y_new = model.predict(y)
-    # print(y[-20:s
-
-    plt.plot(y, color = 'magenta')
-    # plt.plot(x, color = 'magenta')
-    # plt.plot(x_new, color = 'green')
-    plt.show()
-    plt.close()
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from pycoingecko import CoinGeckoAPI
+from datetime import date, datetime
 
 
 def get_historical_data(share_name):
     share = yf.Ticker(share_name)
     historical_data = share.history(period="max")
-    share_info = share.info
 
     data = []
     for row in historical_data.iterrows():
@@ -59,13 +29,74 @@ def get_historical_data(share_name):
             "high": row_data["High"]
         })
 
-    return share_info, data
+    return share.info, data
 
 def get_current_price(symbol):
     ticker = yf.Ticker(symbol)
     todays_data = ticker.history(period='1d')
     return todays_data['Close'][0]
 
-import sys
-share_info, historical_data = get_historical_data(sys.argv[1])
-linear_regression([item["date"] for item in historical_data], [item["close"] for item in historical_data])
+def from_epoch(epochtime):
+    return datetime.fromtimestamp(epochtime / 1000)
+
+def reverse_date_string(date):
+    return "-".join(date.split(" ")[0].split("-")[::-1])
+
+cg = CoinGeckoAPI()
+# prices, market_cap, volume
+data = cg.get_coin_market_chart_by_id('bitcoin', 'usd', 'max')
+
+dates = [from_epoch(i[0]) for i in data["prices"]]
+prices = [i[1] for i in data["prices"]]
+total_volume = [i[1] for i in data["total_volumes"]]
+market_cap = [i[1] for i in data["market_caps"]]
+
+data = [{"date": dates[i], "price": prices[i], "volume": total_volume[i], "market_cap": market_cap[i]} for i in range(len(dates))]
+
+df = pd.DataFrame.from_dict(data, orient='columns')
+df["price"].fillna(method="ffill", inplace=True)
+df["volume"].fillna(method="ffill", inplace=True)
+df["market_cap"].fillna(method="ffill", inplace=True)
+
+
+plt.plot(df["price"], color='blue')
+plt.plot(df["volume"], color='magenta')
+plt.plot(df["market_cap"], color='black')
+
+plt.show()
+
+split_date =  df.index[df['date'] == datetime.strptime("2018-02-25 00:00:00", "%Y-%m-%d %H:%M:%S")].tolist()[0]
+print("splite_date", split_date, df["date"][split_date])
+data_train = df[:split_date].copy()
+data_test = df[split_date:].copy()
+
+# print("len train", len(data_train))
+# print("len test", len(data_test))
+# print("training set", len(data_train.values))
+
+
+
+# split data
+split_date = '25-Jun-2018'
+data_train = data.loc[data.index <= split_date].copy()
+data_test = data.loc[data.index > split_date].copy()
+
+
+
+# print(data_train.head())
+# print(data_test.tail())
+
+training_set = data_train.values
+training_set = np.reshape(training_set, (len(training_set), 1))
+
+sc = MinMaxScaler()
+training_set = sc.fit_transform(training_set)
+X_train = training_set[0:len(training_set) - 1]
+y_train = training_set[1:len(training_set)]
+X_train = np.reshape(X_train, (len(X_train), 1, 1))
+
+
+
+    
+   
+
